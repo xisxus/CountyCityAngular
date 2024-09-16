002e2b516fa800dd6b159dffc7d058e6ef0c14c8
xisxus <shefain3@gmail.com>
2024-09-16 09:27:29 +0600

    ng

--- src/app/Component/home/home.component.ts
+++ src/app/Component/home/home.component.ts
@@ -1,12 +1,33 @@
+// home.component.ts
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  constructor(private router: Router) {}
+
+  navigateAndShowNav() {
+    // Example navigation
+    this.router.navigate(['/ge']);
+  }
+
+  showLoading() {
+    const overlay = document.getElementById('loading-overlay') as HTMLElement;
+    if (overlay) {
+      overlay.style.display = 'flex'; // Show the overlay
+
+     
+      setTimeout(() => {
+        overlay.style.display = 'none'; // Hide the overlay
+        window.location.href = '/some-path'; // Replace with your desired URL
+      }, 2000); // Adjust the timeout duration to match your loading animation
+    }
+  }
+
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'CountyCityPractise';
+
+  showNav = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showNav = event.urlAfterRedirects !== '/';
+      }
+    });
+  }
 }
