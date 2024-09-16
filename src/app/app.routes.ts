011f30059ef306b5cceed21473c86b3b6980eaa1
xisxus <shefain3@gmail.com>
2024-09-16 09:03:17 +0600

    ng

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { GetAllComponent } from './Component/get-all/get-all.component';
 import { createComponent } from '@angular/core';
 import { EditComponent } from './Component/edit/edit.component';
 import { CreateComponent } from './Component/create/create.component';
+import { HomeComponent } from './Component/home/home.component';
 
 export const routes: Routes = [
-    {path : '', component : GetAllComponent},
+    {path : '', component : HomeComponent},
+    {path : 'ge', component : GetAllComponent},
     {path : 'cr', component : CreateComponent},
     {path : 'up/:id', component : EditComponent}
 ];
